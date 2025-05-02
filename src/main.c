37a3e243942bbd6b7678f3df5356d698783a35c8
ryhkml <ryhkmll@gmail.com>
2025-05-02 08:24:27 +0700

    Release v0.1.3

--- src/main.c
+++ src/main.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
                 return EXIT_SUCCESS;
                 break;
             case 'v':
-                printf("0.1.2\n");
+                printf("0.1.3\n");
                 return EXIT_SUCCESS;
             case 0:
                 if (opt_index == 1) ytingest.lang = optarg;
